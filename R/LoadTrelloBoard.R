8f82008895a671773dfc81da019a1bb201c183ef
PeterM74 <60593713+PeterM74@users.noreply.github.com>
2023-05-08 21:12:10 +1000

    Add trello board to profile readme

---
+++ R/LoadTrelloBoard.R
@@ -0,0 +1,132 @@
+library(tidyverse)
+library(httr)
+
+# Load key and token API secrets from Github
+Key <- Sys.getenv("TRELLOKEY")
+Token <- Sys.getenv("TRELLOTOKEN")
+Board <- "6438b578e8f2790b2f230085"
+
+# Load boards - not needed
+# BoardTest <- httr::GET(paste0("https://api.trello.com/1/members/me/boards?key=", Key, "&token=", Token))
+# BoardData <- httr::content(BoardTest)
+
+
+
+# Load lists
+ListTest <- httr::GET(paste0("https://api.trello.com/1/boards/", Board,
+                             "/lists?fields=id,name&key=", Key, 
+                             "&token=", Token))
+
+ListData <- httr::content(ListTest)
+
+ListDF <- purrr::map_dfr(ListData,
+                         function(x) tibble::tibble(id = x$id, name = x$name))
+
+
+
+# Load cards
+CardTest <- httr::GET(paste0("https://api.trello.com/1/boards/", Board, 
+                             "/cards?fields=name,labels,idList&key=", Key,
+                             "&token=", Token))
+
+CardData <- httr::content(CardTest)
+
+CardDF <- purrr::map_dfr(CardData,
+                         function(x) tibble::tibble(id = x$id, 
+                                                    name = x$name, 
+                                                    label = x$labels[[1]]$name, 
+                                                    idList = x$idList)) %>%
+  
+  # Join list name to cards
+  dplyr::left_join(ListDF %>%
+                     dplyr::rename(ListName = name),
+                   by = c("idList" = "id"))
+
+
+
+# Convert into README table
+## Create table columns - generate columns as character vector where each element 
+## is a new line
+ListTitles <- dplyr::pull(ListDF, name)
+ListOfColumns <- purrr::map(ListTitles,
+                            function(ListNameToFilter, CardDF) {
+                              
+                              CardsInList <- CardDF %>%
+                                
+                                dplyr::filter(ListName == ListNameToFilter) %>%
+                                
+                                dplyr::mutate(FullCardDetails = paste0(label,
+                                                                       ": ",
+                                                                       name)) %>%
+                                
+                                dplyr::pull(FullCardDetails)
+                              
+                            }, CardDF = CardDF)
+
+
+## Extend list of columns to be equal and add piping
+MaxListLength <- max(purrr::map_dbl(ListOfColumns, length))
+EqualListOfColumns <- purrr::map(ListOfColumns,
+                                 function(x, MaxLength) {
+                                   
+                                   c(paste0("| ", x, " "), rep("|   ", 
+                                                               MaxListLength - length(x)))
+                                   
+                                 }, MaxLength = MaxListLength)
+
+
+## Header row
+HeaderRow <- purrr::map(ListTitles, 
+                        function(ListName) {
+                          
+                          c(paste0("| ", ListName, " "),
+                            "| --- ")
+                          
+                        })
+
+
+## Combine together
+### Header part
+HeaderTableComponent <- paste0(paste0(purrr::map_chr(HeaderRow, 1), collapse = ""), 
+                               "|\n", 
+                               paste0(purrr::map_chr(HeaderRow, 2), collapse = ""), 
+                               "|\n", 
+                               collapse = "")
+
+
+## Table part
+RowsTableComponent <- purrr::map_chr(1:length(EqualListOfColumns[[1]]),
+                                     function(RowNumber, RowTableData) {
+                                       
+                                       paste0(purrr::map_chr(RowTableData, RowNumber), 
+                                              collapse = "") %>%
+                                         paste0("|\n", collapse = "")
+                                       
+                                     }, RowTableData = EqualListOfColumns) %>%
+  paste0(collapse = "")
+
+
+## Combine into final output
+FinalTableOutput <- paste0("**Personal board**\n", 
+                           HeaderTableComponent, 
+                           RowsTableComponent, 
+                           "*Last updated: ", 
+                           as.character(Sys.time()) %>%
+                             lubridate::as_datetime(tz = Sys.timezone()) %>%
+                             lubridate::with_tz(tzone = "Australia/Sydney"),
+                           "*",
+                           collapse = "")
+
+
+
+# Insert into README
+## Read README
+READMEString <- readLines("README.md")
+
+
+## Insert into README
+READMEString <- gsub(pattern = "<!--TrelloBoard-->.*<!--END-->",
+                     replacement = paste0("<!--TrelloBoard-->", FinalTableOutput, "<!--END-->"),
+                     x = READMEString)
+
+writeLines(READMEString, con = file("README.md"))
--- README.md
+++ README.md
@@ -20,3 +20,7 @@ I'm a **passionate** and experienced health data analyst based in Sydney, Austra
 :memo: Contribute to surgical research<img src="https://projecteuler.net/profile/PeterM74.png" alt="Project Euler Profile" align="right"/>  
 :chess_pawn: Board / video games  
 :trophy: Project Euler problems (wish I had more time to do them!)
+
+Sometimes I lose track of all my personal projects so I use a kanban board to keep on top of it. See my board below to keep track of what is in my pipeline and find out what might be holding up progress on your issue. This is a replica of my private Trello board and is updated via an API every 3 days using GitHub Actions. The Done list is flushed every fortnight.
+
+<!--TrelloBoard-->**Personal board**<!--END-->
